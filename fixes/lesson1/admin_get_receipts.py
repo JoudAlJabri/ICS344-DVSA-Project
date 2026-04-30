f9f889a76a6f1014e4571ae524a949ce9abb6fbd
Joud <s202277140@kfupm.edu.sa>
2026-05-01 00:58:05 +0300

    Delete fixes/lesson1/admin_get_receipts.py

--- fixes/lesson1/admin_get_receipts.py
+++
@@ -1,6 +0,0 @@
-def lambda_handler(event, context):
-    if not event.get("isAdmin") or event.get("isAdmin") != "true":
-        return {
-            "status": "err",
-            "message": "Unauthorized - Admin access required"
-}
